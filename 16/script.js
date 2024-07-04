9c638213fa4770170964a94c51e574eec6665cbb
dinalwr <dianok1105@gmail.com>
2024-07-04 07:47:19 +0700

    four commit

---
+++ 16/script.js
@@ -0,0 +1,5 @@
+// Add event listener to the order button
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', function() {
+  alert('Thank you for your order! Your pizza will be delivered shortly.');
+});
